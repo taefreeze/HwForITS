7bbf9319df671dfd8d540bb5b26c163948e91ba1
taefreeze <potaekingdom@hotmail.com>
2021-01-05 16:31:03 +0700

    add comment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+// นำเข้า LinQ ในการเรียงข้อมูล
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+// นำเข้า  collection เพื่อใช้ในการ สร้าง List
 using System.Collections.Generic;
 
 namespace man
@@ -10,30 +12,39 @@ namespace man
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // สร้าง List ชื่อ "Weight"
             List<string> Weight = new List<string>();
+            // ลำดับคนที่ สร้างมารับ
             int number = 0;
-
+            // initial ของการทำงาน ให้รับค่าแรกของน้ำหนัก
             Console.WriteLine("Please enter an integer");
             string input = Console.ReadLine();
             Weight.Add(input);
+            //ถ้าสูง ไม่ใช่ 170-185
             while (input != "")
             {
+                // รับค่าของคนถัดไป
                 Console.WriteLine("Please enter another integer: ");
                 input = Console.ReadLine();
                 Weight.Add(input);
+                // ลำดับ + 1
                 number += 1;
             }
-
+            //ถ้าสูง 170-185 และส่วนต่าง = 100
             if (input == "")
             {
+                // วนรอบ เอาค่าใน List ออกมา
                 foreach (string value in Weight)
                 {
                     Console.WriteLine(value);
                 }
                 Console.ReadLine();
             }
+            // ทดสอบ เรียงข้อมูลใน List
+            // สร้างตัวแปรเรียงลำดับ
             var DecendingOrder = Weight.OrderByDescending(i => i);
+            // วนรอบ เอาค่าใน List ออกมา (เรียงมากไปน้อยแล้ว)
             foreach(string dinosaur in DecendingOrder)
             {
             Console.WriteLine(dinosaur);
