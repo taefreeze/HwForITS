fd9451df13ba17a255c7727a533a4613eb2099b7
TaeFreeze <potaekingdom@hotmail.com>
2021-01-05 23:20:23 +0700

    finish project :)

--- Program.cs
+++ Program.cs
@@ -15,43 +15,50 @@ namespace man
         {   
             // สร้าง List ชื่อ "Weight"
             List<int> List = new List<int>();
-            List<int> pos = new List<int>();
+
             // ลำดับคนที่ สร้างมารับ
             int order = 1;
+
             // initial ของการทำงาน ให้รับค่าแรกของน้ำหนัก
             Console.WriteLine("Please enter Weight of #"+ order);
             int Weight = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter Height of #" + order);
             int Height = Convert.ToInt32(Console.ReadLine());
+
+            // นำไปใส่ใน List
             List.Add(Weight);
-            pos.Add(order);
 
             //ถ้าสูง ไม่ใช่ 170-185
-            while (Height - Weight != 100)
+            while (Height - Weight != 100 && (Height >= 175 || Height <= 180))
             {
                 // ลำดับ + 1
                 order += 1;
+
                 // รับค่าของคนถัดไป
                 Console.WriteLine("Please enter Weight of #" + order);
                 Weight = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please enter Height of #" + order);
                 Height = Convert.ToInt32(Console.ReadLine());
+
+                // นำไปใส่ใน List
                 List.Add(Weight);
             }
             //ถ้าสูง 170-185 และส่วนต่าง = 100
             if (Height - Weight == 100 && Height >= 175 && Height <= 180)
             {
                 Console.Clear();
+
+                // แสดงข้อมูลคนที่ตรงเงื่อนไข
                 Console.WriteLine("The best is number : " + order + "\nweight = " + Weight + "\nheight = " + Height);
                 Console.WriteLine(new string('-', 50));
 
-
+                // แสดงคนที่ผอมที่สุด และลำดับ
                 int Min = List.Min();
                 int indexMin = List.IndexOf(List.Min()) +1;
                 Console.WriteLine("The slim is number : " + indexMin + "\nweight = " + Min);
                 Console.WriteLine(new string('-', 50));
 
-
+                // แสดงคนที่อ้วนที่สุด และลำดับ
                 int Max = List.Max();
                 int indexMax = List.IndexOf(List.Max()) +1;
                 Console.WriteLine("The fat is number : " + indexMax + "\nweight = " + Max);
